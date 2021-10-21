a7fad1429675f7049a4350edd3747fb5090bbd63
Paritosh Singh <paritosh.singh588@gmail.com>
2021-10-21 13:13:52 +0530

    Third commit

--- index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var Hashmap_1 = require("./Hashmap");
-var main = function () {
-    var mp = new Hashmap_1["default"]();
-    mp.set("8377871347", "Paritosh Singh");
-};
-main();
